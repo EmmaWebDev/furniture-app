56b8c3d0c33df9c66c5396419f92cb65b34cc19b
Emmanuella Amonoo <amonooemma2@gmail.com>
2024-06-28 17:14:21 +0000

    New Update

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -34,24 +34,24 @@ const K = {
             title: "Amazing Deals",
             description: "Far far away, behind the word mountains, far from the countries Vokalia.",
             Icon: <Handshake />,
-            bgColor: "#F5EBEB",
-            iconBg: "#CB9696",
+            bgColor: "#DCEBDD",
+            iconBg: "#8BBD8E",
 
         },
         {
             title: "Amazing Deals",
             description: "Far far away, behind the word mountains, far from the countries Vokalia.",
             Icon: <Handshake />,
-            bgColor: "#F5EBEB",
-            iconBg: "#CB9696",
+            bgColor: "#F5EFD8",
+            iconBg: "#D1B54A",
 
         },
         {
             title: "Amazing Deals",
             description: "Far far away, behind the word mountains, far from the countries Vokalia.",
             Icon: <Handshake />,
-            bgColor: "#F5EBEB",
-            iconBg: "#CB9696"
+            bgColor: "#F4E6D8",
+            iconBg: "#D29A61"
 
         }
 
